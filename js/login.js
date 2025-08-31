68c711e37f13ac7d1f9b9468d8da229599f420fd
rklpoi5678 <rklpoi5678@naver.com>
2025-08-31 17:08:29 +0900

    Revert "Refactor: 함수형코드 변경, gtag 기본이벤트 오타 수정"
    This reverts commit 3546854652c55809ad7eee9407e9d98a1e8145c9.

--- js/login.js
+++ js/login.js
@@ -1,79 +1,89 @@
-import { EmailValidator, PasswordValidator } from "../src/utils/validators.js";
+import { validateEmail, validatePassword } from "../src/utils/validators.js";
 import modalShow from "../src/modal/LoginModal.js";
 import USER_DATA from "../src/db.js";
 
-// DOM이 로드시(방어코드)
-document.addEventListener("DOMContentLoaded", () => {
-  const $email = document.getElementById("email");
-  const $emailError = document.getElementById("email-error");
+const $email = document.getElementById("email");
+const $pw = document.getElementById("password");
+const $submitBtn = document.getElementById("submit-btn");
+const $login = document.getElementById("login");
+const $pwEyeBtn = document.querySelectorAll(".btn_visibility_icon");
 
-  const $pw = document.getElementById("password");
-  const $pwError = document.getElementById("pw-error");
+function checkEmail() {
+  const isPass = validateEmail();
+  return isPass;
+}
 
-  const $submitBtn = document.getElementById("submit-btn");
-  const $login = document.getElementById("login");
-  const $pwEyeBtn = document.querySelectorAll(".btn_visibility_icon");
+function checkPassword() {
+  const isPass = validatePassword();
+  return isPass;
+}
 
-  // 클래스 객체 생성
-  const emailValidator = new EmailValidator($email, $emailError); 
-  const passwordValidator = new PasswordValidator($pw, $pwError);
+/**
+ * 3.    이메일-비밀번호 유효성 검사
+ * 3-3.  로그인 버튼 활성화 여부
+ * 3-4.  더미 데이터 검증
+ */
+function checkFormValid() {
+  const isValid = checkEmail() && checkPassword();
+  // !true로 토글
+  $submitBtn.disabled = !isValid;
+}
 
-  /**
-   *    이메일-비밀번호 유효성 검사
-   */
-  function checkFormValid() {
-    const isValid = emailValidator.validate() && passwordValidator.validate();
-    $submitBtn.disabled = !isValid;
+function userLoginAuth(email, password) {
+  const user = USER_DATA.find(
+    (user) => (user.email === email) & (user.password === password)
+  );
+  if (!user) return "비밀번호가 일치하지 않습니다";
+  return "";
+}
+
+// 이벤트리스너 등록 - 이벤트 발생시 호출
+$email.addEventListener("focusout", () => {
+  checkEmail();
+  checkFormValid();
+});
+
+$pw.addEventListener("focusout", () => {
+  checkPassword();
+  checkFormValid();
+});
+
+/**
+ * 4. 제출후 페이지 이동
+ *
+ * @event submit
+ * @param {SubmitEvent} e - 폼 제출 이벤트 객체
+ *
+ */
+$login.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = $email.value;
+  const password = $pw.value;
+
+  const authMessage = userLoginAuth(email, password);
+
+  if (authMessage) {
+    modalShow(authMessage);
+  } else {
+    location.href = "/items";
   }
+});
 
-  $email.addEventListener("focusout", () => {
-    emailValidator.validate();
-    checkFormValid();
-  });
+/**
+ * 5. 비밀번호 표시 on/off
+ */
+$pwEyeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const wrapper = btn.closest(".password-wrapper");
+    const input = wrapper.querySelector("input");
+    const icon = wrapper.querySelector("img");
 
-  $pw.addEventListener("focusout", () => {
-    passwordValidator.validate();
-    checkFormValid();
-  });
-
-  /**
-   * @event submit
-   * @param {SubmitEvent} e - 폼 제출 이벤트 객체
-   *
-   */
-  $login.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = $email.value;
-    const password = $pw.value;
-
-    const user = USER_DATA.find(
-      (user) => user.email === email && user.password === password
-    );
-
-
-    if (!user) {
-      modalShow("비밀번호가 일치하지 않습니다.");
+    if (input.type === "password") {
+      input.type = "text";
+      icon.src = "./public/images/btn_visibility_on.svg";
     } else {
-      location.href = "/items";
+      input.type = "password";
+      icon.src = "./public/images/btn_visibility_off.svg";
     }
-  });
-
-  /**
-   * 5. 비밀번호 표시 on/off
-   */
-  $pwEyeBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const wrapper = btn.closest(".password-wrapper");
-      const input = wrapper.querySelector("input");
-      const icon = wrapper.querySelector("img");
-
-      if (input.type === "password") {
-        input.type = "text";
-        icon.src = "./public/images/btn_visibility_on.svg";
-      } else {
-        input.type = "password";
-        icon.src = "./public/images/btn_visibility_off.svg";
-      }
-    });
   });
 });
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,155 +1,104 @@
+const $id = document.getElementById("email");
+const $idError = document.getElementById("emailError");
+
+const $pw = document.getElementById("password");
+const $pwError = document.getElementById("pwError");
+
+const $pwCheker = document.getElementById("pw-check");
+const $pwChekerError = document.getElementById("pwErrorCheck");
+
+const $nickName = document.getElementById("nickname");
+const $nickNameError = document.getElementById("nickNameError");
+
+const PW_CHECK_LENGTH = 8;
+const MIN_NAME_LENGTH = 2;
+
+const EMAIL_REG = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 /**
- * 유효성 검사 부모 클래스
- * - 필드 검증 클래스 상속자 역활
- * - 기능: 에러 메시지 표시, 값가져오기
+ * 1.     검증 이메일
+ * 1-1.   공백  제거 포함 이메일 형식 검증 입력값을 검증하고 오류 메시지를 표시
+ * @returns {boolean}
  */
-class Validator {
-  constructor($input, $error) {
-    this.$input = $input;
-    this.$error = $error;
-  }
-
-  // 앞뒤 공백을 제거한 값을 가져온다.
-  get Value() {
-    return this.$input.value.trim();
-  }
-
-  showError(msg) {
-    this.$error.textContent = msg;
-    this.$input.classList.add("input-error");
-  }
-
-  clearError() {
-    this.$error.textContent = "";
-    this.$input.classList.remove("input-error");
-  }
-
-  // export 한 코드만 사용
-  validate() {
-    throw new Error("해당 코드는 하위 클래스에서 구현이 되어야 합니다");
+export function validateEmail() {
+  const value = $id.value.trim();
+  const isValid = EMAIL_REG.test(value);
+  if (isValid) {
+    $idError.textContent = "";
+    $id.classList.remove("input-error");
+    return true;
+  } else {
+    $idError.textContent = !value
+      ? ($idError.textContent = "이메일을 입력해주세요.")
+      : ($idError.textContent = "잘못된 이메일 형식입니다.");
+    $id.classList.toggle("input-error", !false);
+    return false;
   }
 }
 
 /**
- * 이메일 형식 검증
- * (부모 클래스의 Value로)공백 제거후 이메일 정규식으로 형식을 확인합니다.
- *
- * @class
- * @param {HTMLElement} $email
- * @param {HTMLElement} $emailError
- * @returns {Boolean}
+ * 2.     공백 제거한 비밀번호를, 비밀 번호 길이 검증
+ * 2-1.   빈문자열 , 8자리이하 false리턴
+ * @returns {boolean}
  */
-export class EmailValidator extends Validator {
-  validate() {
-    const EMAIL_REG = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const value = this.Value;
-
-    if (!value) {
-      this.showError("이메일을 입력해주세요.");
-      return false;
-    }
-
-    if (!EMAIL_REG.test(value)) {
-      this.showError("잘못된 이메일 형식입니다.");
-      return false;
-    }
-
-    this.clearError();
+export function validatePassword() {
+  const value = $pw.value.trim();
+  const isValid = value.length >= PW_CHECK_LENGTH;
+  // console.log(isVaild)
+  if (isValid) {
+    $pwError.textContent = "";
+    $pw.classList.remove("input-error");
     return true;
+  } else {
+    $pwError.textContent = !value
+      ? ($pwError.textContent = "비밀번호를 입력해주세요.")
+      : ($pwError.textContent = `비밀번호는 ${PW_CHECK_LENGTH}자 이상 입력해주세요.`);
+    $pw.classList.toggle("input-error", !false);
+    return false;
   }
 }
 
 /**
- * 닉네임 검증
- * @param {HTMLElement} $nickname
- * @param {HTMLElement} $nicknameError
- * @param {Number} minLength(옵션)
- * @returns {Boolean}
+ * 3.     닉네임 검증
+ * @returns {boolean}
  */
-export class NicknameValidator extends Validator {
-  constructor($input, $error, minLength = 2) {
-    super($input, $error);
-    this.minLength = minLength;
-  }
-
-  validate() {
-    const value = this.Value;
-
-    if (!value) {
-      this.showError("닉네임을 입력해주세요");
-      return false;
-    }
-
-    if (value.length < this.minLength) {
-      this.showError(`닉네임은 ${this.minLength}자 이상 입력해주세요.`);
-      return false;
-    }
-
-    this.clearError();
+export function validateNickName() {
+  const value = $nickName.value.trim();
+  const isValid = value.length >= MIN_NAME_LENGTH;
+  if (isValid) {
+    $nickNameError.textContent = "";
+    $nickName.classList.remove("input-error");
     return true;
+  } else {
+    $nickNameError.textContent = !value
+      ? ($nickNameError.textContent = "닉네임을 입력해주세요.")
+      : ($nickNameError.textContent = `닉네임은 ${MIN_NAME_LENGTH}자 이상 입력해주세요.`);
+    $nickName.classList.toggle("input-error", !false);
+    return false;
   }
 }
 
 /**
- * 패스워드 검증
- *
- * @param {HTMLElement} $pw
- * @param {HTMLElement} $pwError
- * @param {Number} minLength-(옵션)
- * @returns {Boolean}
+ * 4.     검증 이메일
+ *        빈값일 경우, 현재 패스워드 비교
+ * @returns {boolean}
  */
-export class PasswordValidator extends Validator {
-  constructor($input, $error, minLength = 8) {
-    super($input, $error);
-    this.minLength = minLength;
-  }
-
-  validate() {
-    const value = this.Value;
-
-    if (!value) {
-      this.showError("비밀번호를 입력해주세요.");
-      return false;
-    }
-
-    if (value.length < this.minLength) {
-      this.showError(`비밀번호는 ${this.minLength}자 이상 입력해주세요`);
-      return false;
-    }
-
-    this.clearError();
+export function validatePasswordCheker() {
+  const value = $pwCheker.value.trim();
+  const isValid = Boolean(value) && value === $pw.value.trim();
+  console.log(value);
+  console.log($pw.value.trim());
+  if (isValid) {
+    console.log(isValid);
+    $pwChekerError.textContent = "";
+    $pwCheker.classList.remove("input-error");
     return true;
-  }
-}
-
-/**
- *  패스워드 체커
- * @param {HTMLElement} $pwCheker
- * @param {HTMLElement} $pwChekerError
- * @param {HTMLElement} $pw
- * @returns {Boolean}
- */
-export class PasswordCheckValidator extends Validator {
-  constructor($input, $error, $compare) {
-    super($input, $error);
-    this.$compare = $compare;
-  }
-
-  validate() {
-    const value = this.Value;
-    const origin = this.$compare.value.trim();
-
-    if (!value) {
-      this.showError("비밀번호를 입력해주세요.");
-      return false;
-    }
-
-    if (value !== origin) {
-      this.showError("비밀번호가 일치하지 않습니다.");
-      return false;
-    }
-
-    this.clearError();
-    return true;
+  } else {
+    $pwChekerError.textContent =
+      !value || isValid
+        ? ($pwChekerError.textContent = "비밀번호를 입력해주세요.")
+        : ($pwChekerError.textContent = `비밀번호가 일치하지 않습니다.`);
+    $pwCheker.classList.toggle("input-error", !false);
+    return false;
   }
 }
